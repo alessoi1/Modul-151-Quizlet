c02ff8a325cbb44940e8a096a79f65012b957174
noserronnie <93775605+noserronnie@users.noreply.github.com>
2022-10-08 19:58:15 +0200

    change design of learnset details page, use german everywhere, test learnset bugfix

---
+++ Quizleter/ViewModels/LearnsetDetailsViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Quizleter.ViewModels
+{
+    public class LearnsetDetailsViewModel
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Creator { get; set; }
+        public int VocabCount { get; set; }
+    }
+}
